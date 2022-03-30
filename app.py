4ca5ea26b2dea9ac581e1736be86da2e588366ea
CharlieWarrior <18005@wc.school.nz>
2022-03-31 10:44:23 +1300

    Home Page Working

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+from sqlite3 import Error
+from datetime import datetime
+
+app = Flask(__name__)
+DATABASE = "smile.db"
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
+
+@app.route('/')
+def render_home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run()
